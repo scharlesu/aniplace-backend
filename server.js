42c7e49114f78f5e160ad0ee1722c7eddbe37ec9
scharlesu <scharlesu@gmail.com>
2024-12-05 01:00:37 -0500

    Initial commit

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require("express");
 const app = express();
 
 app.listen(5000, () => {
-  console.log("Sever started on port 5000");
+  console.log("Server started on port 5000");
 });
